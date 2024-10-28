b392654582a884dd154a9db85a6ce38b97768c8f
Ezeholo <129399436+Ezeholo@users.noreply.github.com>
2024-10-28 20:18:14 -0300

    Add files via upload

---
+++ Menu.py
@@ -0,0 +1,12 @@
+
+print("Menu gestion de opciones de producto\n")
+print("1- Productos  ")
+print("2- Gestion de stock ")
+print("3- Alta de stock  ")
+print("4- Baja de Stock ")
+print("5- Eliminar producto ")
+print("0- Salir  ")
+
+opcion_menu = input("ingresa una opcion ") #retorna un str
+
+print(f"La opcion seleccionada es {opcion_menu}")
